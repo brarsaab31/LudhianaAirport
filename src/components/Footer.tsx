0ac3fac0cbda668649b0ec6ead3990c06469a8a9
brarsaab31 <cuisineinternational100@gmail.com>
2025-07-30 16:49:18 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
 
   const socialLinks = [
     { icon: Facebook, href: 'https://www.facebook.com/HalwaraAirport', label: 'Facebook' },
-    { icon: Twitter, href: '#', label: 'Twitter' },
+    { icon: Twitter, href: 'https://x.com/halwaraairport', label: 'Twitter' },
     { icon: Instagram, href: 'https://www.instagram.com/ludhianaairport/', label: 'Instagram' },
     { icon: Linkedin, href: '#', label: 'LinkedIn' }
   ];
