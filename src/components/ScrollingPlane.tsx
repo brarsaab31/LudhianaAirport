9c31ccd51052034a8b77cf0b03487479e7749e8a
brarsaab31 <cuisineinternational100@gmail.com>
2025-08-03 10:45:21 +0200

    Updated ScrollingPlane.tsx

--- src/components/ScrollingPlane.tsx
+++ src/components/ScrollingPlane.tsx
@@ -46,7 +46,7 @@ const ScrollingPlane = () => {
           <div className="relative">
             {/* Plane icon - solid filled, rotated to point down */}
             <Plane 
-              className="h-8 w-8 text-blue-600 transform rotate-90" 
+              className="h-8 w-8 text-blue-600 transform rotate-180" 
               fill="currentColor"
             />
           </div>
