9bb2d60ed588a0aea10ae2743969f8a775b1ad59
brarsaab31 <cuisineinternational100@gmail.com>
2026-05-25 10:15:06 +0200

    Updated Disclaimer.tsx

--- src/pages/Disclaimer.tsx
+++ src/pages/Disclaimer.tsx
@@ -19,7 +19,7 @@ const Disclaimer = () => {
       title: 'Risk Management',
       description: 'Protect against uncertainties',
       image: 'https://images.pexels.com/photos/1591447/pexels-photo-1591447.jpeg?auto=compress&cs=tinysrgb&w=800',
-      website: 'https://www.policybazaar.com',
+      website: 'https://clnk.in/AWIr',
       color: 'bg-red-600'
     },
     {
